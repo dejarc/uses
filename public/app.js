a566c0e1c5acc6673c03ecbc4e0a3e3ba8021e67
Aaron Chau <aaronchauchau@gmail.com>
2016-10-15 14:56:37 -0700

    Added code for streaming jpegs.

--- public/app.js
+++ public/app.js
@@ -107,4 +107,27 @@ angular.module('myApp', [
 .factory("CurrentUserRef", ["$firebaseAuth", function($firebaseAuth) {
     return firebase.database().ref('users/' + $firebaseAuth().$getAuth().uid);
   }
-]);
+])
+.factory('socket', function ($rootScope) {
+  var socket = io.connect();
+  return {
+    on: function (eventName, callback) {
+      socket.on(eventName, function () {  
+        var args = arguments;
+        $rootScope.$apply(function () {
+          callback.apply(socket, args);
+        });
+      });
+    },
+    emit: function (eventName, data, callback) {
+      socket.emit(eventName, data, function () {
+        var args = arguments;
+        $rootScope.$apply(function () {
+          if (callback) {
+            callback.apply(socket, args);
+          }
+        });
+      })
+    }
+  };
+});
--- public/controller/dashboard.js
+++ public/controller/dashboard.js
@@ -2,10 +2,37 @@
  
 angular.module('myApp.dashboard', ['ngRoute'])
 // Home controller
-.controller('DashboardCtrl', ["$scope", "$firebaseArray", "CurrentUserRef", 
-    function($scope, $firebaseArray, CurrentUserRef) {
+.controller('DashboardCtrl', ["$scope", "$firebaseArray", "Auth", "CurrentUserRef", "socket", 
+    function($scope, $firebaseArray, Auth, CurrentUserRef, socket) {
         var modulesRef = CurrentUserRef.child('modules');
         $scope.modules = $firebaseArray(modulesRef);
+        $scope.isLive = false;
+        $scope.liveurl = "";
+
+        $scope.toggleLiveStream = function() {
+            if ($scope.isLive) {
+                socket.emit('stop-stream', {
+                    uid: Auth.$getAuth().uid
+                });
+            } else {
+                socket.emit('start-stream', {
+                    uid: Auth.$getAuth().uid
+                });
+            }
+            $scope.isLive = !$scope.isLive;
+        }
+
+        $scope.pauseLiveStream = function() {
+            $scope.isLive =false;
+        }
+
+        socket.on('newImage', function(streamData) {
+            $scope.liveurl = streamData.url;
+            socket.emit('deleteImage', {
+                uid: Auth.$getAuth.uid,
+                data: streamData
+            });
+        });
 
         /* Use this for logs
         // create a query for the most recent 25 messages on the server
