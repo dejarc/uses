7e5bdcfe577307a658877495fa0a4a81c4a3ffe7
Can Huynh <Can Huynh>
2016-10-15 01:47:27 -0700

    Testing push messages on server.

--- public/controller/settings.js
+++ public/controller/settings.js
@@ -4,4 +4,19 @@ angular.module('myApp.settings', ['ngRoute'])
 // Home controller
 .controller('SettingsCtrl', ["$scope", function($scope) {
 	var firebaseObj = firebase.database().ref();
+	
+	$scope.pushButton = function() {
+		if (isPushEnabled) {
+		  unsubscribe();
+		} else {
+		  subscribe();
+		}
+	};
+
+	// Check that service workers are supported, if so, progressively
+	// enhance and add push messaging support, otherwise continue without it.
+	if ('serviceWorker' in navigator) {
+		navigator.serviceWorker.register('pushjs/service-worker.js').then(initialiseState);
+	}
 }]);
+
